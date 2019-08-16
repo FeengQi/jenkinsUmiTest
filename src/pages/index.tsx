c798355f32ce572ca9a5617ec7d8261eefd748dd
fengqi <fengqi_bj@ysstech.com>
2019-08-16 17:02:33 +0800

    test CI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import styles from './index.css';
 import { formatMessage } from 'umi-plugin-locale';
 
- export default function() 
-{
+export default function() {
   return (
     <div className={styles.normal}>
       <div className={styles.welcome} />
